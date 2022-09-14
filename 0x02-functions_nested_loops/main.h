fe9ce7def356bde4fb40ea37d4574dfdf48139ed
Wolleys <wolleysmigaya@gmail.com>
2022-09-14 16:49:28 +0000

    Print the n times table, starting with 0 > If n is greater than 15 or less than 0 the function should not print anything

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,7 @@
  * times_table - prints times table up to 9
  * add - add two ints, return sum
  * print_to_98 - prints all natural numbers to 98
+ * print_times_table - print times table up to
 
  */
 
@@ -28,4 +29,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int);
+void print_times_table(int n);
 
