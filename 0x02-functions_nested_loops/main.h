625f632122b108faf3f841c6aeaf8e446c936097
Wolleys <wolleysmigaya@gmail.com>
2022-09-14 12:46:08 +0000

    Check for lowercase character > Returns 1 if c is lowercase > Returns 0 otherwise

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,9 +4,11 @@
  * _putchar - writes the character c to stdout
  *  print_alphabet - prints alphabet and returns new line
  *  print_alphabet_x10 - print alphabet 10 times
+ * _islower - return 1 if c is lowercase, 0 otherwise
  *
  */
 
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int);
