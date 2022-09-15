34f6cb7b83ba3afa37b0ea243c1191fe319a6ba5
Wolleys <wolleysmigaya@gmail.com>
2022-09-15 11:21:38 +0000

    Print the numbers, from 0 to 9 > You can only use _putchar twice in your code

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,11 +6,15 @@
 * _isupper - checks if char is capital letter
 * _isdigit - checks if int is a digit 0-9
 * mul - multiply two numbers
+* print_numbers - print numbers 0-9
+* _putchar - writes the character c to stdout
 
 */
 
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+void print_numbers(void);
+int _putchar(char);
 
 #endif /* MAIN_H */
