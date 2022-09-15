900d524060730da39ae1874cb0075409669cef6c
Wolleys <wolleysmigaya@gmail.com>
2022-09-15 11:50:46 +0000

    Print 10 times the numbers, from 0 to 14 > Followed by a new line > can only use _putchar three times in your code

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,6 +9,7 @@
 * print_numbers - print numbers 0-9
 * _putchar - writes the character c to stdout
 * print_most_numbers - print 0-9 w/out 2 and 4
+* more_numbers - print 0-14 ten times
 
 **/
 
@@ -18,5 +19,6 @@ int mul(int, int);
 void print_numbers(void);
 int _putchar(char);
 void print_most_numbers(void);
+void more_numbers(void);
 
 #endif /* MAIN_H */
