c2c6ecc98c8d5489ab4e856eb284f85ba2fa8837
Wolleys <wolleysmigaya@gmail.com>
2022-09-15 11:29:51 +0000

    Print the numbers, from 0 to 9 > Do not print 2 and 4 > You can only use _putchar twice in your code

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,13 +8,15 @@
 * mul - multiply two numbers
 * print_numbers - print numbers 0-9
 * _putchar - writes the character c to stdout
+* print_most_numbers - print 0-9 w/out 2 and 4
 
-*/
+**/
 
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
 int _putchar(char);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
