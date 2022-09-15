65994c77a654751005b831f2caea16e83a35d635
Wolleys <wolleysmigaya@gmail.com>
2022-09-15 10:58:01 +0000

    Check for a digit (0 through 9) > Returns 1 if c is a digit > Returns 0 otherwise

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,10 +4,11 @@
 #include <stdio.h>
 /**
 * _isupper - checks if char is capital letter
+* _isdigit - checks if int is a digit 0-9
 
 */
 
 int _isupper(int);
-
+int _isdigit(int);
 
 #endif /* MAIN_H */
