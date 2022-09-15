a83f9769ed59b316a712146381af738cd12de666
Wolleys <wolleysmigaya@gmail.com>
2022-09-15 11:06:44 +0000

    Multiply two integers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,10 +5,12 @@
 /**
 * _isupper - checks if char is capital letter
 * _isdigit - checks if int is a digit 0-9
+* mul - multiply two numbers
 
 */
 
 int _isupper(int);
 int _isdigit(int);
+int mul(int, int);
 
 #endif /* MAIN_H */
