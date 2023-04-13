38bcc1769938f7b18dbd7ad1ab4d69771b7f6b3f
Wolleys <wolleysmigaya@gmail.com>
2023-04-13 11:06:32 +0000

    Search for a value in a sorted array of integers
    > Using the Jump search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 /**
  * struct listint_s - singly linked list
@@ -41,6 +42,6 @@ typedef struct skiplist_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 
 #endif
